70c29feaf2d13fcf5a258eb2e94c821303ce9672
Xavier Binois <xbinois@harlequin.rp.u-bourgogne.fr>
2020-01-06 17:51:07 +0100

    add Testinfra Tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,12 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+def test_sshd_is_installed(host):
+    sshd = host.package("ssh")
+    assert sshd.is_installed
+
+def test_sshd_is_running_and_enabled(host):
+    sshd = host.service("ssh")
+    assert sshd.is_running
+    assert sshd.is_enabled

8c916486cd8bb583ab5c975888b3fbd7fd4c29a8
Xavier Binois <xbinois@harlequin.rp.u-bourgogne.fr>
2020-01-07 10:27:34 +0100

    Syntax corrections

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,10 +6,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-ansible_variables = host.ansible.get_variables()
-
-
 def test_myvar_using_get_variables(host):
+    ansible_variables = host.ansible.get_variables()
     assert 'sshd_service_name' in ansible_variables
     assert 'sshd_packages' in ansible_variables
 
@@ -23,11 +21,13 @@ def test_hosts_file(host):
 
 
 def test_sshd_is_installed(host):
+    ansible_variables = host.ansible.get_variables()
     sshd = ansible_variables['sshd_packages']
     assert sshd.is_installed
 
 
 def test_sshd_is_running_and_enabled(host):
+    ansible_variables = host.ansible.get_variables()
     sshd = ansible_variables['sshd_service_name']
     assert sshd.is_running
     assert sshd.is_enabled
